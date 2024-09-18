78760e9d9e3adbe80251d79e9d26407a3a059d21
Yinan Y <yangyinanapp@gmail.com>
2024-09-18 14:06:58 -0400

    Fixing csv reading issue

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-df=pd.read_csv('vehicles_us_utf8.csv',encoding='utf-8')
+df=pd.read_csv('vehicles_us_utf8.csv',encoding='utf-8-sig')
 
 
 
